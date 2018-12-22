46f0778b3df1eca7eede2aad120e164f45a05088
Paul Liu <pstar.liu@gmail.com>
2018-12-22 17:58:12 +1300

    Updates

---
+++ precache-manifest.77cb43e722d06078f8a7e22c8b11d67f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc47854eb7294afd3ce8",
+    "url": "/mygh-website/static/css/main.a8ffe0cf.chunk.css"
+  },
+  {
+    "revision": "fc47854eb7294afd3ce8",
+    "url": "/mygh-website/static/js/main.fc47854e.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/mygh-website/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "bde03d742af3011dccbd",
+    "url": "/mygh-website/static/js/runtime~main.bde03d74.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mygh-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8e6d0c47ea84434e7a59064d460786e7",
+    "url": "/mygh-website/index.html"
+  }
+];
